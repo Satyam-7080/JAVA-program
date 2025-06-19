1c04a9266cfb05ecb5f8f991453a95b304e15a3c
Satyam Dubey <152508966+Satyam-7080@users.noreply.github.com>
2025-06-19 13:16:25 +0530

    Update _21Check_Even_Odd_by_Bitwise_Operator.java

--- _21Check_Even_Odd_by_Bitwise_Operator.java
+++ _21Check_Even_Odd_by_Bitwise_Operator.java
@@ -1,13 +1,20 @@
+//Even Odd number by using bitwise operator 
+
 import java.util.Scanner;
 public class _21Check_Even_Odd_by_Bitwise_Operator {
+
+    //Main Mehtod 
+    
     public static void main(String[] args) {
         System.out.println("Check the number is even or odd using bitwise operator: ");
         System.out.print("Please enter the number: ");
         Scanner input = new Scanner(System.in);
         long num = input.nextLong();
+        
+       // Bitwise operator ( & ) If-Else Statement
+        
         if ((num & 1) == 1)
         {
-
             System.out.println(num + " is odd number.");
         } else
         {
